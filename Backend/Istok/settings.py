570e3e44eeda6a6c815818f4aeb54f7c3aa3ec40
MyrsinSergey <myrsin.s@ya.ru>
2024-05-29 21:54:58 +0400

    Подключение базы данных PostgreSQL и создание приложени я Istok_app в проекте

--- Backend/Istok/settings.py
+++ Backend/Istok/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Istok.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.getenv("Istok_DB_NAME"),
+    'USER': os.getenv("Istok_DB_LOGIN"),
+    'PASSWORD': os.getenv("Istok_DB_PASS"),
+    'HOST': os.getenv("Istok_DB_HOST"),
+    'PORT': os.getenv("Istok_DB_PORT"),
     }
 }
 
