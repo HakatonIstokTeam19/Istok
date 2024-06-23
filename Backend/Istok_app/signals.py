90398faa588c985dbb7f23431acc08a96a6dbf72
MyrsinSergey <myrsin.s@ya.ru>
2024-06-23 17:10:51 +0400

    добавил отправку почты при создании заявки

--- Backend/Istok_app/signals.py
+++ Backend/Istok_app/signals.py
@@ -1,8 +1,9 @@
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from users.models import Profile, Loyalty
-from .models import Orders
+from .models import Orders, Application
 from django.core.exceptions import ObjectDoesNotExist
 
 
@@ -17,3 +18,29 @@ def create_profile(sender, instance, created, **kwargs):
             if orders_quantity == 1 and loyalty:
                 if order_user != loyalty.user:
                     loyalty.increase_balance(instance.order_price)
+
+@receiver(post_save, sender=Application)
+def send_application_email(sender, instance, created, **kwargs):
+    if created:
+        subject = 'Новая заявка'
+        message = f'''
+        New application details:
+        Тип мебели: {instance.type}
+        Форма мебели: {instance.form}
+        Дополнения: {instance.addition}
+        Материал фасада: {instance.facades_material}
+        Материал столешницы: {instance.table_material}
+        Кухонная сантехника: {instance.plumb}
+        Бытовая техника: {instance.appliances}
+        Бюджет проекта: {instance.budget}
+        Консультация с экспертом по обустройству дома: {instance.consultation}
+        Фамилия: {instance.last_name}
+        Имя: {instance.first_name}
+        Отчество: {instance.patronymic}
+        Ваш номер телефона: {instance.phone}
+        Как с Вами связаться?: {instance.connection}
+        Ваша ссылка на Telegram/ВКонтакте: {instance.link}
+        Дата: {instance.data}
+        Время: {instance.time}
+        '''
+        send_mail(subject, message, 'myrsin.s@yandex.com', ['myrsin.s@yandex.com'])
